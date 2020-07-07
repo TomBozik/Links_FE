46a6d9e2202d28047f6b74a71fdf68cd4acbd3cd
TomBozik <tombozik@gmail.com>
2020-07-07 23:12:19 +0200

    add dist

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 let Api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    // baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://links-api.tmbo.sk/api',
     withCredentials: true
 });
 
