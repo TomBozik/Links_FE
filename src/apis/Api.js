67d09c60fce0601c6cc6c335a9c82d60a3aefffb
TomBozik <tombozik@gmail.com>
2020-07-10 15:46:34 +0200

    https api

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 let BaseApi = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    // baseURL: 'http://links-api.tmbo.sk/api',
+    // baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://links-api.tmbo.sk/api',
 });
 
 let Api = function(){
     let token = localStorage.getItem('token');
-    
+
     if(token){
         BaseApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     }
